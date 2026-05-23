e098d126bf633674fa27a9807cd2e036ec3641a7
bando9 <bandomega1@gmail.com>
2026-05-23 23:41:34 +0700

    chore: nonaktif theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import { Geist, Geist_Mono, Roboto } from "next/font/google"
 
 import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+// import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@/lib/utils"
 
-const roboto = Roboto({subsets:['latin'],variable:'--font-sans'})
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -20,10 +20,17 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", roboto.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        roboto.variable
+      )}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        {/* <ThemeProvider> */}
+        {children}
+        {/* </ThemeProvider> */}
       </body>
     </html>
   )
